4d265ef651e92844b7ad169ced2ec15aaed9f1e4
Andrew Wasem <drewwasem@gmail.com>
2011-09-28 16:22:30 -0400

    DOC: Added more description to itk-module.cmake
    Change-Id: Ic5108db08ead061e88beb6d38556b1666f723ccd

--- Modules/ThirdParty/DCMTK/itk-module.cmake
+++ Modules/ThirdParty/DCMTK/itk-module.cmake
@@ -1,6 +1,5 @@
 set(DOCUMENTATION "This module contains the third party <a
-href=\"http://dicom.offis.de/dcmtk/\">DCMTK</a> library.
-")
+href=\"http://dicom.offis.de/dcmtk/\">DCMTK</a> DCMTK is a collection of libraries and applications implementing large parts the DICOM standard.")
 
 itk_module(ITKDCMTK
   DEPENDS
